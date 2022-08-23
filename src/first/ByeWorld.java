48d89b608f1a9f62e65ce13ecd2ef9dae7229a11
IlyaK <ilyakulidi@gmail.com>
2022-08-23 11:31:29 -0400

    Added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("May be 5 weeks of Java left");
+        System.out.println("1.5 weeks of Git");
+    }
+}
